ee875b2c8632b9f39b626a2be5b6f5476b4d4c22
lawwayne <72894553+lawwayne@users.noreply.github.com>
2022-04-29 15:26:50 -0400

    Time: 76 ms (65.71%), Space: 43 MB (57.72%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    //TWO POINTERS SOLUTION
+    let i = 0
+    let j = numbers.length - 1
+    
+    while(i != j){
+        if(numbers[i] + numbers[j] > target){
+            j--;
+        }
+        else if(numbers[i] + numbers[j] < target){
+            i++;
+        }
+        else{
+            return [i+1,j+1];
+        }
+    }
+};
