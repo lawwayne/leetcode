fbdc844cacd5035144ce9e8653ae18c5fe3f6e27
lawwayne <72894553+lawwayne@users.noreply.github.com>
2022-05-13 12:08:23 -0400

    Update 20-valid-parentheses.py

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -13,6 +13,10 @@ class Solution:
                 stack.append(c)
         return True if not stack else False
             
+# The Time Complexity is O(n) as for any given string we are taking a linear search approach. 
+# The Space Complexity is also O(n) as the stack does take a bit of memory and it is necessary in order to keep track of each parentheses value.
+# For the lower bound it would be Ω(1) and that is only if the string itself would be empty. 
+
             
         
 
